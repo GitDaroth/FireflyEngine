0c9167f9477efd6a6bbdceff51a5b7ab75b70593
Daroth <fabian.gulde@gmx.de>
2020-06-05 17:08:42 +0200

    Added assimp lib to the project for model loading

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,22 +1,20 @@
 #include <FireflyEngine.h>
 
-#include <glm/gtc/matrix_transform.hpp>
-
 #include "CameraController.h"
 
-using namespace Firefly;
+#include <glm/gtc/matrix_transform.hpp>
 
-class SandboxApp : public Application
+class SandboxApp : public Firefly::Application
 {
 public:
 	SandboxApp()
 	{
-		m_camera = std::make_shared<Camera>(m_window->GetWidth(), m_window->GetHeight());
+		m_camera = std::make_shared<Firefly::Camera>(m_window->GetWidth(), m_window->GetHeight());
 		m_camera->SetPosition(glm::vec3(0.f, 0.f, 2.f));
 
 		m_cameraController = std::make_shared<CameraController>(m_camera);
 
-		m_renderer = std::make_unique<Renderer>();
+		m_renderer = std::make_unique<Firefly::Renderer>();
 
 		float vertices[5 * 4] = {
 			-0.5f, -0.5f, 0.f, 0.f, 0.f,
@@ -24,26 +22,26 @@ public:
 			 0.5f,  0.5f, 0.f, 1.f, 1.f,
 			-0.5f,  0.5f, 0.f, 0.f, 1.f
 		};
-		std::shared_ptr<VertexBuffer> vertexBuffer = RenderingAPI::CreateVertexBuffer();
+		std::shared_ptr<Firefly::VertexBuffer> vertexBuffer = Firefly::RenderingAPI::CreateVertexBuffer();
 		vertexBuffer->Init(vertices, sizeof(vertices));
 		vertexBuffer->SetLayout({
-			{ ShaderDataType::Float3, "position" },
-			{ ShaderDataType::Float2, "texCoord" }
+			{ Firefly::ShaderDataType::Float3, "position" },
+			{ Firefly::ShaderDataType::Float2, "texCoord" }
 			});
 
 		uint32_t indices[6] = { 0, 1, 2, 2, 3, 0 };
-		std::shared_ptr<IndexBuffer> indexBuffer = RenderingAPI::CreateIndexBuffer();
+		std::shared_ptr<Firefly::IndexBuffer> indexBuffer = Firefly::RenderingAPI::CreateIndexBuffer();
 		indexBuffer->Init(indices, sizeof(indices));
 
-		m_vertexArray = RenderingAPI::CreateVertexArray();
+		m_vertexArray = Firefly::RenderingAPI::CreateVertexArray();
 		m_vertexArray->Init();
 		m_vertexArray->AddVertexBuffer(vertexBuffer);
 		m_vertexArray->SetIndexBuffer(indexBuffer);
 
-		m_shader = RenderingAPI::CreateShader();
+		m_shader = Firefly::RenderingAPI::CreateShader();
 		m_shader->Init("assets/shaders/test.glsl");
 
-		m_texture = RenderingAPI::CreateTexture2D();
+		m_texture = Firefly::RenderingAPI::CreateTexture2D();
 		m_texture->Init("assets/textures/test.jpg");
 
 		m_shader->Bind();
@@ -59,8 +57,8 @@ protected:
 	{
 		m_cameraController->OnUpdate(deltaTime);
 
-		RenderingAPI::GetRenderFunctions()->SetClearColor(glm::vec4(0.1f, 0.1f, 0.1f, 1.f));
-		RenderingAPI::GetRenderFunctions()->Clear();
+		Firefly::RenderingAPI::GetRenderFunctions()->SetClearColor(glm::vec4(0.1f, 0.1f, 0.1f, 1.f));
+		Firefly::RenderingAPI::GetRenderFunctions()->Clear();
 
 		m_renderer->BeginScene(m_camera);
 		m_texture->Bind(0);
@@ -70,9 +68,9 @@ protected:
 		m_renderer->EndScene();
 	}
 
-	virtual void OnWindowEvent(std::shared_ptr<WindowEvent> event) override
+	virtual void OnWindowEvent(std::shared_ptr<Firefly::WindowEvent> event) override
 	{
-		if (auto resizeEvent = event->AsType<WindowResizeEvent>())
+		if (auto resizeEvent = event->AsType<Firefly::WindowResizeEvent>())
 		{
 			int width = resizeEvent->GetWidth();
 			int height = resizeEvent->GetHeight();
@@ -85,25 +83,25 @@ protected:
 		}
 	}
 
-	virtual void OnKeyEvent(std::shared_ptr<KeyEvent> event) override
+	virtual void OnKeyEvent(std::shared_ptr<Firefly::KeyEvent> event) override
 	{
 
 	}
 
-	virtual void OnMouseEvent(std::shared_ptr<MouseEvent> event) override
+	virtual void OnMouseEvent(std::shared_ptr<Firefly::MouseEvent> event) override
 	{
 		m_cameraController->OnMouseEvent(event);
 	}
 
-	std::unique_ptr<Renderer> m_renderer;
-	std::shared_ptr<Camera> m_camera;
+	std::unique_ptr<Firefly::Renderer> m_renderer;
+	std::shared_ptr<Firefly::Camera> m_camera;
 	std::shared_ptr<CameraController> m_cameraController;
-	std::shared_ptr<Shader> m_shader;
-	std::shared_ptr<Texture2D> m_texture;
-	std::shared_ptr<VertexArray> m_vertexArray;
+	std::shared_ptr<Firefly::Shader> m_shader;
+	std::shared_ptr<Firefly::Texture2D> m_texture;
+	std::shared_ptr<Firefly::VertexArray> m_vertexArray;
 };
 
-Application* Firefly::InstantiateApplication()
+Firefly::Application* Firefly::InstantiateApplication()
 {
 	return new SandboxApp();
 }
